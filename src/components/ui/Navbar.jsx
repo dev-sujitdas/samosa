196552ceec7770869ed432b057356f82290d44e7
dev-sujitdas <dev.sujitdas@gmail.com>
2025-12-30 23:46:57 +0530

    navbar fix

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -5,17 +5,18 @@ import { MdClose } from "react-icons/md";
 import { Link } from 'react-router-dom';
 import BoardGame from '../../pages/BoardGame';
 import ContactUs from '../../pages/Contact';
+import { div } from 'motion/react-client';
 
 
 const Navbar = ({ openForm }) => {
   const [isOpen, setIsOpen] = useState(false);
 
- const menus = [
-  { label: "Book Table", type: "action", onClick: openForm },
-  { label: "Our Games", type: "route", to: "/boardgame" },
-  { label: "Our Menus", type: "ahref", target: "menus" },
-  { label: "Contact Us", type: "route", to: "/contact" },
-];
+  const menus = [
+    { label: "Book Table", type: "action", onClick: openForm },
+    { label: "Our Games", type: "route", to: "/boardgame" },
+    { label: "Our Menus", type: "ahref", target: "menus" },
+    { label: "Contact Us", type: "route", to: "/contact" },
+  ];
 
 
   const clickHandler = () => {
@@ -31,57 +32,59 @@ const Navbar = ({ openForm }) => {
   };
 
   return (
-    <nav className='navbar w-full fixed top-0 z-999'>
-      <div className='w-full max-w-600 mx-auto px-5 md:px-12 lg:px-16  bg-[#2f4a2c52] backdrop-blur-2xl relative'>
-        <div className='flex justify-between items-center border-b py-4 border-[#16a1557e] '>
-          <div><a href="/"><img className='h-10 lg:h-16 width-fit' src="/logo2-bg.png" alt="" /></a></div>
-          <div onClick={clickHandler}><h2 className='flex items-center gap-1 text-[#F6A230] text-xl md:text-2xl poppins-semibold cursor-pointer'><FiMenu />MENU</h2></div>
-          <AnimatePresence>
-            {isOpen && (
-              <motion.div
-                initial={{ x: "100%" }}
-                animate={{ x: 0, opacity: 1 }}
-                exit={{ x: "100%", }}
-                transition={{ duration: 0.45, ease: "backOut" }}
-                className='h-dvh md:h-screen w-full md:w-1/2 xl:w-[35%] bg-[#F6A230] absolute top-0 right-0 rounded-2xl p-5 flex flex-col shadow-xl'
-              >
-                <div className='w-full flex justify-end'>
-                  <div onClick={clickHandler} className='h-14 w-14 bg-zinc-100 shadow-lg rounded-full flex justify-center items-center hover:scale-95 duration-300 cursor-pointer'>
-                    <h2 className='text-4xl'><MdClose /></h2>
+    <nav className='w-full overflow-hidden'>
+      <div className='navbar w-full fixed top-0 z-999'>
+        <div className='w-full max-w-600 mx-auto px-5 md:px-12 lg:px-16  bg-[#2f4a2c52] backdrop-blur-2xl relative'>
+          <div className='flex justify-between items-center border-b py-4 border-[#16a1557e] '>
+            <div><a href="/"><img className='h-10 lg:h-16 width-fit' src="/logo2-bg.png" alt="" /></a></div>
+            <div onClick={clickHandler}><h2 className='flex items-center gap-1 text-[#F6A230] text-xl md:text-2xl poppins-semibold cursor-pointer'><FiMenu />MENU</h2></div>
+            <AnimatePresence>
+              {isOpen && (
+                <motion.div
+                  initial={{ x: "100%" }}
+                  animate={{ x: 0, opacity: 1 }}
+                  exit={{ x: "100%", }}
+                  transition={{ duration: 0.45, ease: "backOut" }}
+                  className='h-dvh md:h-screen w-full md:w-1/2 xl:w-[35%] bg-[#F6A230] absolute top-0 right-0 rounded-2xl p-5 flex flex-col shadow-xl'
+                >
+                  <div className='w-full flex justify-end'>
+                    <div onClick={clickHandler} className='h-14 w-14 bg-zinc-100 shadow-lg rounded-full flex justify-center items-center hover:scale-95 duration-300 cursor-pointer'>
+                      <h2 className='text-4xl'><MdClose /></h2>
+                    </div>
                   </div>
-                </div>
-                <div className="w-full h-full flex flex-col justify-center items-center gap-10">
-                  {menus.map((menu, index) => {
-                    const Item = (
-                      <motion.h2
-                        key={menu.label}
-                        initial={{ x: "50%", opacity: 0 }}
-                        animate={{ x: 0, opacity: 1 }}
-                        transition={{ delay: index * 0.05 }}
-                        onClick={() => handleMenuClick(menu)}
-                        whileHover={{ scale: 1.05 }}
-                        whileTap={{ scale: 0.97 }}
-                        className={`text-3xl md:text-4xl lg:text-5xl xl:text-6xl px-6 py-4 rounded-full bg-zinc-100 shadow-lg cursor-pointer`}
-                      >
-                        {menu.label}
-                      </motion.h2>
-                    );
-
-                    if (menu.type === "route") {
-                      return (
-                        <Link key={menu.label} to={menu.to}>
-                          {Item}
-                        </Link>
+                  <div className="w-full h-full flex flex-col justify-center items-center gap-10">
+                    {menus.map((menu, index) => {
+                      const Item = (
+                        <motion.h2
+                          key={menu.label}
+                          initial={{ x: "50%", opacity: 0 }}
+                          animate={{ x: 0, opacity: 1 }}
+                          transition={{ delay: index * 0.05 }}
+                          onClick={() => handleMenuClick(menu)}
+                          whileHover={{ scale: 1.05 }}
+                          whileTap={{ scale: 0.97 }}
+                          className={`text-3xl md:text-4xl lg:text-5xl xl:text-6xl px-6 py-4 rounded-full bg-zinc-100 shadow-lg cursor-pointer`}
+                        >
+                          {menu.label}
+                        </motion.h2>
                       );
-                    }
 
-                    return Item;
-                  })}
-                </div>
+                      if (menu.type === "route") {
+                        return (
+                          <Link key={menu.label} to={menu.to}>
+                            {Item}
+                          </Link>
+                        );
+                      }
 
-              </motion.div>
-            )}
-          </AnimatePresence>
+                      return Item;
+                    })}
+                  </div>
+
+                </motion.div>
+              )}
+            </AnimatePresence>
+          </div>
         </div>
       </div>
     </nav>
