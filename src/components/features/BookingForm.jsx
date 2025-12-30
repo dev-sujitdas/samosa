9a2f998a26d2c6593501a9b5e55d31f756ba5a06
dev-sujitdas <dev.sujitdas@gmail.com>
2025-12-30 23:50:31 +0530

    update

--- src/components/features/BookingForm.jsx
+++ src/components/features/BookingForm.jsx
@@ -4,7 +4,7 @@ import { IoClose } from "react-icons/io5";
 const BookingForm = ({ closeForm }) => {
 
   return (
-    <section className='fixed top-0  w-full h-dvh flex justify-center items-center z-999 bg-[#31303054] backdrop-blur-md '>      
+    <section className='fixed top-0  w-full h-dvh flex justify-center items-center z-999 bg-[#ececec] backdrop-blur-md '>      
       <div className='w-full h-full flex flex-col justify-center items-center relative'>
         <div className='w-1/2 flex justify-end absolute right-3 top-2 bg-[#ececec]'>
         <button onClick={closeForm} className='rounded-full p-2 bg-white text-black font-bold  text-xl shadow-lg hover:scale-105 cursor-pointer'><IoClose /></button>
